0aa842608dee20985f6c172cac418be13c24b8dd
wildchair <tonkikh.danila@yandex.ru>
2025-04-11 02:41:08 +0300

    Update LessonNew.cs

--- ScheduleCore/Models/LessonNew.cs
+++ ScheduleCore/Models/LessonNew.cs
@@ -5,11 +5,11 @@ namespace ScheduleCore.Models
     public class LessonNew
     {
         public int Id { get; set; }
-        public string Classroom { get; set; }
+        public string? Classroom { get; set; }
         public required string Type { get; set; }
-        public string Time { get; set; }
+        public string? Time { get; set; }
         public required string Name { get; set; }
-        public TypeOfWeek Week { get; set; }
+        public required TypeOfWeek Week { get; set; }
 
 
         //public string? CardDay { get; set; }
