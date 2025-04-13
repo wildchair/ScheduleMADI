3665afdf2b58970e54254e1f66952bd8c0d7df72
wildchair <93913475+wildchair@users.noreply.github.com>
2025-04-14 00:09:24 +0300

    Update Lesson.cs

--- ScheduleCore/Models/Lesson.cs
+++ ScheduleCore/Models/Lesson.cs
@@ -8,8 +8,8 @@
         public TimeSpan Start { get; set; }
         public TimeSpan End { get; set; }
         public string? Type { get; set; }
-        public required List<Group> Groups { get; set; }
-        public required List<Professor> Professors { get; set; }
+        public List<Group> Groups { get; set; } = [];
+        public List<Professor> Professors { get; set; } = [];
         public DayOfWeek Day { get; set; }
         public string? Classroom { get; set; }
     }
