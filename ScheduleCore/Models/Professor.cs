ffe5bb6027917e050f67fb9fa4970f264d20a01a
wildchair <93913475+wildchair@users.noreply.github.com>
2025-04-14 00:08:38 +0300

    Update Professor.cs

--- ScheduleCore/Models/Professor.cs
+++ ScheduleCore/Models/Professor.cs
@@ -4,7 +4,7 @@
     {
         public required int Id { get; set; }
         public required string Name { get; set; }
-        public required List<Lesson> Lessons { get; set; }
+        public List<Lesson> Lessons { get; set; } = [];
         //public required List<Examination> Exams { get; set; }
     }
 }
